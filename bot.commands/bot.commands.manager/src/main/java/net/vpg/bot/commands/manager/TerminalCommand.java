5e70ccde53cb8699659b07c076005c01bf0d08b4
V-Play-Games <vaibhavnargwani28@gmail>
2021-11-12 16:11:13 +0530

    Fixed Copyright Spacing

--- bot.commands/bot.commands.manager/src/main/java/net/vpg/bot/commands/manager/TerminalCommand.java
+++ bot.commands/bot.commands.manager/src/main/java/net/vpg/bot/commands/manager/TerminalCommand.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +15,11 @@
  */
 package net.vpg.bot.commands.manager;
 
+import net.vpg.bot.framework.Bot;
 import net.vpg.bot.framework.commands.BotCommandImpl;
+import net.vpg.bot.framework.commands.CommandReceivedEvent;
 import net.vpg.bot.framework.commands.ManagerCommand;
 import net.vpg.bot.framework.commands.NoArgsCommand;
-import net.vpg.bot.framework.Bot;
-import net.vpg.bot.framework.commands.CommandReceivedEvent;
 
 public class TerminalCommand extends BotCommandImpl implements NoArgsCommand, ManagerCommand {
     public TerminalCommand(Bot bot) {
